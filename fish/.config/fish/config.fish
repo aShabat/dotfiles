9abf054a99207f223b1709d336c661d1628725b2
Anton Shabat <anton.shabat@gmail.com>
2024-04-27 14:18:55 +0300

    FISH: some variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -g EDITOR nvim
+set -g VISUAL nvim
+
 abbr --add lg lazygit
 
 zoxide init fish --cmd cd | source
