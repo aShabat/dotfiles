ae3b97ae0d696a5dfa91a6a376b07aed24fb15c2
Anton Shabat <anton.shabat@gmail.com>
2024-05-01 14:35:32 +0300

    NVIM: stuff

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -40,6 +40,19 @@ return ({
                     use_as_default_explorer = true,
                 },
             })
+            local files_set_cwd = function (path)
+                print("changed")
+                local cur_entry_path = MiniFiles.get_fs_entry().path
+                local cur_directory = vim.fs.dirname(cur_entry_path)
+                vim.fn.chdir(cur_directory)
+            end
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "MiniFilesBufferCreate",
+                callback = function (args)
+                    vim.keymap.set("n", "g~", files_set_cwd, { buffer = args.data.buf_id })
+                end,
+            })
 
             require("mini.hipatterns").setup({
                 highlighters = {
