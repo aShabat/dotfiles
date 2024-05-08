1276f54df0b2f34fb6c21fbea4cbc3c643b56314
Anton Shabat <anton.shabat@gmail.com>
2024-05-08 07:47:31 +0300

    NVIM: tweaking focus

--- neovim/.config/nvim/lua/plugins/focus.lua
+++ neovim/.config/nvim/lua/plugins/focus.lua
@@ -3,6 +3,7 @@ return {
         "nvim-focus/focus.nvim",
         opts = {
             ui = {
+                hybridnumber = true,
                 absolutenumber_unfocussed = true,
                 colorcolumn = {
                     enable = true,
