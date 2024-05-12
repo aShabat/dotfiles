7aa2618e215ff3b0a254a1edcafee6c3068bda2f
Anton Shabat <anton.shabat@gmail.com>
2024-05-13 00:08:47 +0300

    NVIM: gitsigns

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -7,6 +7,13 @@ return {
     },
     {
         "lewis6991/gitsigns.nvim",
-        opts = {},
+        opts = {
+            current_line_blame = true,
+            current_line_blame_opts = {
+                delay = 0,
+                ignore_whitespace = true,
+            },
+            numhl = true,
+        },
     },
 }
--- neovim/.config/nvim/lua/user/keymaps.lua
+++ neovim/.config/nvim/lua/user/keymaps.lua
@@ -35,7 +35,7 @@ vim.keymap.set("n", "<leader>tz", require("telescope").extensions.zoxide.list)
 
 -- LazyGit
 
-vim.keymap.set("n", "<leader>git", "<CMD>LazyGit<CR>")
+vim.keymap.set("n", "<leader>gs", "<CMD>LazyGit<CR>")
 
 -- MiniTrailSpace
 
