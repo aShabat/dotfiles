2b7a84b4ea158b2bc55eb1088971a473dcc21cab
Anton Shabat <anton.shabat@gmail.com>
2024-05-29 13:53:55 +0300

    WEZTERM: moved files around

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,8 +1,20 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
-local config = require("config")
+local config = {}
 
-local colors, _ = wezterm.color.load_scheme("/home/anton/.config/wezterm/config/theme.toml")
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+local colors, _ = wezterm.color.load_scheme("/home/anton/.config/wezterm/base16-theme.toml")
 config.colors = colors
+
+config.font = wezterm.font 'JetBrainsMono Nerd Font Mono'
+
+config.keys = {
+    { key = "PageUp", mods = "SHIFT", action = act.ScrollByPage(-0.5) },
+    { key = "PageDown", mods = "SHIFT", action = act.ScrollByPage(0.5) }
+}
 
 return config
