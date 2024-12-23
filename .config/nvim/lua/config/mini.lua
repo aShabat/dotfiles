85f00a011b4d31c07c66883c0ec0d0ceadb88052
Anton Shabat <anton.shabat@gmail.com>
2024-12-23 12:09:04 +0200

    mini base16 and files

--- .config/nvim/lua/config/mini.lua
+++ .config/nvim/lua/config/mini.lua
@@ -1,3 +1,18 @@
+-- Base16
 if vim.fn.filereadable(vim.fn.stdpath('config') .. '/lua/config/minibase16.lua') ~= 0 then
     dofile(vim.fn.stdpath('config') .. '/lua/config/minibase16.lua')
 end
+
+-- Files
+require'mini.files'.setup{
+    options = {
+        use_as_default_explorer = true,
+    },
+    windows = {
+        width_preview = 100,
+    },
+    mappings = {
+        go_in = 'L',
+        go_in_plus = 'l',
+    },
+}
