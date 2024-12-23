9b12e6b5d428e52e76fda861931544444406dc16
Anton Shabat <anton.shabat@gmail.com>
2024-12-23 22:19:54 +0200

    cmp config

--- .config/nvim/lua/config/plugins/cmp.lua
+++ .config/nvim/lua/config/plugins/cmp.lua
@@ -5,7 +5,11 @@ return {
         'rafamadriz/friendly-snippets',
     },
     opts = {
-        keymap = { preset = 'default' },
+        keymap = {
+            preset = 'default',
+            ['<C-l>'] = { 'snippet_forward' },
+            ['<C-h>'] = { 'snippet_backward' },
+        },
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = 'mono',
@@ -13,6 +17,12 @@ return {
         sources = {
             default = { 'lsp', 'path', 'snippets', 'buffer' },
         },
+        completion = {
+            keyword = { range = 'prefix' },
+            accept = { auto_brackets = { enabled = false } },
+            documentation = { auto_show = true },
+        },
+        signature = { enabled = true },
     },
     opts_extend = { 'sources.default' },
 }
