1cd9b5c47ffeb984cc2aebfd0d80abc18e2ebb2e
Anton Shabat <anton.shabat@gmail.com>
2024-12-23 13:09:42 +0200

    lua_ls formatting settings

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -1,26 +1,26 @@
 return {
-	'neovim/nvim-lspconfig',
-	dependencies = {
-		'saghen/blink.cmp',
-		{
-			'folke/lazydev.nvim',
-			opts = {
-				library = {
-					path = { "${3rd}/luv/library", words = {"vim%.uv"} },
-				},
-			},
-		},
-	},
-	opts = {
-		servers = {
-			lua_ls = {},
-		},
-	},
-	config = function(_, opts) 
-		local lspconfig = require'lspconfig'
-		for server, config in pairs(opts.servers) do
-			config.capabilities = require'blink.cmp'.get_lsp_capabilities(config.capabilities)
-			lspconfig[server].setup(config)
-		end
-	end,
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        'saghen/blink.cmp',
+        {
+            'folke/lazydev.nvim',
+            opts = {
+                library = {
+                    path = { '${3rd}/luv/library', words = { 'vim%.uv' } },
+                },
+            },
+        },
+    },
+    opts = {
+        servers = {
+            lua_ls = {},
+        },
+    },
+    config = function (_, opts)
+        local lspconfig = require'lspconfig'
+        for server, config in pairs(opts.servers) do
+            config.capabilities = require'blink.cmp'.get_lsp_capabilities(config.capabilities)
+            lspconfig[server].setup(config)
+        end
+    end,
 }
