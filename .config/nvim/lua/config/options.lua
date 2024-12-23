d040ffef47053f8ec861f4b8a7943aed08a58760
Anton Shabat <anton.shabat@gmail.com>
2024-12-23 11:42:09 +0200

    options

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.scrolloff = 10
