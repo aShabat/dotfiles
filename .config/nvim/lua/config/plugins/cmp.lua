996654692f6d8c1620a1c0ca13ef2454c09b53b7
Anton Shabat <anton.shabat@gmail.com>
2024-12-25 16:34:04 +0200

    nvim tweak blink.cmp

--- .config/nvim/lua/config/plugins/cmp.lua
+++ .config/nvim/lua/config/plugins/cmp.lua
@@ -2,7 +2,8 @@ return {
     'Saghen/blink.cmp',
     version = '*',
     dependencies = {
-        'rafamadriz/friendly-snippets',
+        -- 'rafamadriz/friendly-snippets',
+        'echasnovski/mini.nvim',
     },
     opts = {
         keymap = {
@@ -15,17 +16,44 @@ return {
             nerd_font_variant = 'mono',
         },
         sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
+            default = { 'lsp', 'path', 'buffer' },
         },
         completion = {
             keyword = { range = 'prefix' },
             accept = { auto_brackets = { enabled = false } },
             documentation = { auto_show = true },
             list = { selection = 'auto_insert' },
+            menu = {
+                draw = {
+                    columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'kind' } },
+                    components = {
+                        kind_icon = {
+                            ellipsis = false,
+                            text = function (ctx)
+                                local kind_icon, _, _ = require'mini.icons'.get('lsp', ctx.kind)
+                                return kind_icon
+                            end,
+                            highlight = function (ctx)
+                                local _, hl, _ = require'mini.icons'.get('lsp', ctx.kind)
+                                return hl
+                            end,
+                        },
+                        kind = {
+                            text = function (ctx)
+                                return '< ' .. ctx.kind .. ' >'
+                            end,
+                            highlight = function (ctx)
+                                local _, hl, _ = require'mini.icons'.get('lsp', ctx.kind)
+                                return hl
+                            end,
+                        },
+                    },
+                },
+            },
         },
         signature = {
             enabled = true,
-            window = { direction_priority = { 's', 'n' } },
+            window = { direction_priority = { 'n', 's' } },
         },
     },
     opts_extend = { 'sources.default' },
