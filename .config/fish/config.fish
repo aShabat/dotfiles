f9bbdc8fe5f6c0305d7e1d2b5204adda7a1680bf
Anton Shabat <anton.shabat@gmail.com>
2024-12-26 10:25:58 +0200

    nvimpager as default pager

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 set -gx EDITOR nvim
 set -g VISUAL nvim
 set -gx TERMINAL wezterm
+set -gx PAGER nvimpager
 set PATH $PATH /home/anton/.local/bin /home/anton/.cargo/bin
 set -g GOPATH /home/anton/.local
 
@@ -8,6 +9,8 @@ abbr -a lg lazygit
 abbr -a s systemctl
 abbr -a c config
 abbr -a ca config add
+abbr -a np nvimpager
+abbr -a n nvim
 
 bind --erase --all
 source ~/.config/fish/keys.fish
