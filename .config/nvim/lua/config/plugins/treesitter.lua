06b5922582cf3e8a7c7644825dd80bbab6245b9e
Anton Shabat <anton.shabat@gmail.com>
2024-12-29 16:52:45 +0200

    nvim added incremental selection and indent to treesitter config

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -7,6 +7,17 @@ return {
             highlight = {
                 enable = true,
             },
+            indent = {
+                enable = true,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    node_incremental = '+',
+                    scope_incremental = '*',
+                    node_decremental = '-',
+                },
+            },
         },
     },
     {
