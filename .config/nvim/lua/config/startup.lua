d5aab1c08a7002e474fe599d1067bc287ef7592d
Anton Shabat <anton.shabat@gmail.com>
2024-12-29 22:38:53 +0200

    nvim updated startup script to check that the buffer is empty before opening directory picker

--- .config/nvim/lua/config/startup.lua
+++ .config/nvim/lua/config/startup.lua
@@ -1,4 +1,6 @@
 -- Start directory picker if in empty buffer
-if vim.fn.getreg'%' == '' then
-    require'config.mini'.pick_files{ dirs = true }
-end
+vim.schedule(function ()
+    if next(vim.fn.argv()) == nil and vim.api.nvim_buf_get_lines(0, 0, 1, false)[1] == '' then
+        require'config.mini'.pick_files{ dirs = true }
+    end
+end)
