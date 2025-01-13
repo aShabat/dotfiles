fe2ee90c38ebfeac8730d33b2f7b114a50de9e61
Anton Shabat <anton.shabat@gmail.com>
2025-01-13 22:18:18 +0200

    nvim CONFORM DIDN'T WORK BECAUSE I LOST AN 'S'

--- .config/nvim/lua/config/plugins/conform.lua
+++ .config/nvim/lua/config/plugins/conform.lua
@@ -1,16 +1,16 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {
-        formatter_by_ft = {
-            lua = { 'stylua' },
-            -- go = { 'gofmt' },
-        },
-        default_format_opts = {
-            lsp_format = 'fallback',
-        },
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-        },
-    },
+	'stevearc/conform.nvim',
+	opts = {
+		formatters_by_ft = {
+			lua = { 'stylua' },
+			cmake = { 'gersemi' },
+		},
+		default_format_opts = {
+			lsp_format = 'fallback',
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = 'fallback',
+		},
+	},
 }
