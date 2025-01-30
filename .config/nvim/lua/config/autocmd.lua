415a2faef789d909a88c74e2ee11f57607acc645
Anton Shabat <anton.shabat@gmail.com>
2025-01-30 17:39:15 +0200

    nvim added autocmd to disable retative numbers when in another window

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -1,3 +1,5 @@
+local H = {}
+
 vim.api.nvim_create_autocmd('BufWritePre', {
 	pattern = '*.go',
 	callback = function()
@@ -18,5 +20,28 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 			end
 		end
 		vim.lsp.buf.format { async = false }
+	end,
+})
+
+-- Disable relative numbers when in other window
+
+H.relnum_disabled = {}
+vim.api.nvim_create_autocmd('WinLeave', {
+	callback = function(args)
+		local win = vim.api.nvim_get_current_win()
+		if vim.wo[win].relativenumber then
+			H.relnum_disabled[win] = true
+			vim.wo[win].relativenumber = false
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd('WinEnter', {
+	callback = function(_)
+		local win = vim.api.nvim_get_current_win()
+		if H.relnum_disabled[win] then
+			vim.wo[win].relativenumber = true
+			H.relnum_disabled[win] = false
+		end
 	end,
 })
