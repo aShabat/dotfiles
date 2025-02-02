1249d290d4237495fb55d7b9ec8e34c93444ba4e
Anton Shabat <anton.shabat@gmail.com>
2025-02-02 09:41:22 +0200

    fish added abbr to run g++ with all needed flags

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,7 @@ abbr -a c config
 abbr -a ca config add
 abbr -a np nvimpager
 abbr -a n nvim
+abbr -a cpp g++ -std=c++17 -ggdb -pedantic-errors -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion -Werror
 
 set -g fish_key_bindings set_custom_keys
 
