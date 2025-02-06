69a5d81b82fd968f12f23297be29718ada604a8d
Anton Shabat <anton.shabat@gmail.com>
2025-02-06 20:35:02 +0200

    nvim MiniAI replaced G with builtin in MiniExtra and added line

--- .config/nvim/lua/config/mini.lua
+++ .config/nvim/lua/config/mini.lua
@@ -341,14 +341,8 @@ require('mini.splitjoin').setup {}
 require('mini.ai').setup {
     custom_textobjects = {
         -- Whole region
-        G = function()
-            local from = { line = 1, col = 1 }
-            local to = {
-                line = vim.fn.line '$',
-                col = math.max(vim.fn.getline('$'):len(), 1),
-            }
-            return { from = from, to = to }
-        end,
+        G = MiniExtra.gen_ai_spec.buffer(),
+        L = MiniExtra.gen_ai_spec.line(),
     },
 }
 
