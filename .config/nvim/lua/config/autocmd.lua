9bd93136408a2b9b263cba8e7a8dba73f3555e83
Anton Shabat <anton.shabat@gmail.com>
2025-02-11 10:38:17 +0200

    nvim moved startup module to autocmd on VimEnter

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -1,5 +1,15 @@
 local H = {}
 
+-- Enter triggers
+
+vim.api.nvim_create_autocmd('VimEnter', {
+    callback = function(args)
+        --     if next(vim.fn.argv()) == nil and vim.api.nvim_buf_get_lines(0, 0, 1, false)[1] == '' then
+        --         MiniPick.registry.files { dirs = true }
+        --     end
+    end,
+})
+
 -- Disable relative numbers when in other window
 
 H.relnum_disabled = {}
