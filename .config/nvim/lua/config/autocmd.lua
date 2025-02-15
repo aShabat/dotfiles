75b9cf7fcf757c4229b1efc66656abaca7733b19
Anton Shabat <anton.shabat@gmail.com>
2025-02-15 17:05:38 +0200

    nvim disabled opening dir picker on empty enter

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -2,16 +2,16 @@ local H = {}
 
 -- Enter triggers
 
-vim.api.nvim_create_autocmd('VimEnter', {
-    callback = function(args)
-        vim.schedule(function()
-            if vim.fn.argc() == 0 and vim.api.nvim_buf_get_lines(0, 0, 1, false)[1] == '' then
-                vim.bo.buflisted = false
-                MiniPick.registry.files { dirs = true }
-            end
-        end)
-    end,
-})
+-- vim.api.nvim_create_autocmd('VimEnter', {
+--     callback = function(args)
+--         vim.schedule(function()
+--             if vim.fn.argc() == 0 and vim.api.nvim_buf_get_lines(0, 0, 1, false)[1] == '' then
+--                 vim.bo.buflisted = false
+--                 MiniPick.registry.files { dirs = true }
+--             end
+--         end)
+--     end,
+-- })
 
 -- Disable relative numbers when in other window
 
