24b2c396bac5235b38807accae774456fc54f03e
Anton Shabat <anton.shabat@gmail.com>
2025-02-18 16:35:00 +0200

    nvim movet git keymaps and config to separate module

--- .config/nvim/lua/config/plugins/git.lua
+++ .config/nvim/lua/config/plugins/git.lua
@@ -1,28 +1,9 @@
-local H = {}
-H.saved_git_on_attach = function(_) end
-
 return {
     'lewis6991/gitsigns.nvim',
-    opts = {
-        worktrees = {
-            {
-                toplevel = vim.env.HOME,
-                gitdir = vim.env.HOME .. '/.config/dotfiles.git',
-            },
-        },
-        -- signcolumn = false,
-        numhl = true,
-        diff_opts = {
-            algorithm = 'histogram',
-        },
-        on_attach = function(buf)
-            require('config.plugins.git').git_on_attach(buf)
-        end,
+    dependencies = {
+        'nvimtools/hydra.nvim',
     },
-    git_on_attach = function(buf)
-        H.saved_git_on_attach(buf)
-    end,
-    set_git_on_attach = function(func)
-        H.saved_git_on_attach = func
+    config = function()
+        require 'config.git'
     end,
 }
