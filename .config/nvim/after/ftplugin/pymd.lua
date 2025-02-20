f3c1b95cb48c2202b3ad6bb0a7241b6bef3a3485
Anton Shabat <anton.shabat@gmail.com>
2025-02-20 13:37:10 +0200

    fixup! nvim pymd tweaking

--- .config/nvim/after/ftplugin/pymd.lua
+++ .config/nvim/after/ftplugin/pymd.lua
@@ -1,3 +1,4 @@
+vim.opt.scrolloff = 2
 require('quarto').activate()
 
 require('config.plugins.lsp').lsp_keymaps {}
@@ -11,20 +12,23 @@ end
 
 set(']c', function()
     if MiniAi.find_textobject('a', 'c', { search_method = 'next' }) then
-        MiniAi.move_cursor('left', 'i', 'c', { search_method = 'next' })
+        MiniAi.move_cursor('right', 'i', 'c', { search_method = 'next' })
     else
         vim.api.nvim_buf_set_lines(0, -1, -1, false, { '```python', '', '```', '' })
         vim.api.nvim_win_set_cursor(0, { vim.api.nvim_buf_line_count(0) - 2, 0 })
     end
+    vim.api.nvim_input 'zz'
 end, 'next [C]ell')
 set('[c', function()
-    MiniAi.move_cursor('left', 'i', 'c', { search_method = 'prev' })
+    MiniAi.move_cursor('right', 'i', 'c', { search_method = 'prev' })
+    vim.api.nvim_input 'zz'
 end, 'prev [C]ell')
 
 set('<leader>re', '<CMD>MoltenEvaluateOperator<CR>', '[R]unner [E]valuate')
 set('<leader>rr', '<CMD>MoltenReevaluateCell<CR>', '[R]unner [R]eevaluate')
 set('<leader>rmd', '<CMD>MoltenDelete<CR>', '[R]unner [M]olten [D]elete')
 set('<leader>rR', '<CMD>MoltenRestart!<CR>', '[R]unner [R]estart')
+set('<leader>rS', '<CMD>MoltenInterupt<CR>', '[R]unner [S]top')
 set('<leader>oc', '<CMD>MoltenHideOutput<CR>', '[O]utput [C]lose')
 set('<leader>oe', 'zt<CMD>sleep 10m<CR><CMD>noautocmd MoltenEnterOutput<CR>', '[O]utput [E]nter')
 set('<leader>oi', '<CMD>MoltenImagePopup<CR>', '[O]utput [I]mage')
