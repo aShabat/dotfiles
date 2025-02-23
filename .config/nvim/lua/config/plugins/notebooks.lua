801e3903f6bb4a9761658ab1148b89b49410b2f0
Anton Shabat <anton.shabat@gmail.com>
2025-02-23 18:08:52 +0200

    nvim molten tweaking

--- .config/nvim/lua/config/plugins/notebooks.lua
+++ .config/nvim/lua/config/plugins/notebooks.lua
@@ -6,7 +6,7 @@ return {
             vim.g.molten_auto_open_output = true
             vim.g.molten_image_provider = 'image.nvim'
             vim.g.molten_image_location = 'float'
-            vim.g.molten_wrap_output = true
+            vim.g.molten_wrap_output = false
             vim.g.molten_virt_text_output = true
             vim.g.molten_virt_lines_off_by_1 = true
         end,
