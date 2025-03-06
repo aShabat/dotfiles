61b281ec4e3b2568576cd75fa4c43e6629d3f96a
Anton Shabat <anton.shabat@gmail.com>
2025-03-06 09:27:57 +0200

    fish some abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,12 @@ abbr -a np nvimpager
 abbr -a n nvim
 abbr -a cpp g++ -std=c++17 -ggdb -pedantic-errors -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion -Werror
 
+abbr -a o xdg-open
+abbr -a open xdg-open
+
+abbr -a snapl sudo snapper list -a --disable-used-space
+abbr -a snapls sudo snapper list -a
+
 set -g fish_key_bindings set_custom_keys
 
 zoxide init fish --cmd cd | source
