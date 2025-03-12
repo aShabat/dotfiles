ce00d37f110a1bc77c4c8ecd549b6456ecfe6c3b
Anton Shabat <anton.shabat@gmail.com>
2025-03-12 11:22:10 +0200

    fixup! nvim changed python notebook framework to NotebookNavigator

--- .config/nvim/lua/config/plugins/notebooks.lua
+++ .config/nvim/lua/config/plugins/notebooks.lua
@@ -26,7 +26,7 @@ return {
             vim.g.molten_image_location = 'float'
             vim.g.molten_wrap_output = false
             vim.g.molten_virt_text_output = true
-            vim.g.molten_virt_lines_off_by_1 = true
+            -- vim.g.molten_virt_lines_off_by_1 = true
         end,
         config = function()
             vim.keymap.set('n', '<leader>rI', '<CMD>MoltenInit<CR>', { desc = '[R]unner [I]nit' })
