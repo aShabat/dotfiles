8bbae0e8556347577e34b6f26455b1c409ec71e4
Anton Shabat <anton.shabat@gmail.com>
2025-03-17 23:13:20 +0200

    nvim removed clang-tidy for cpp

--- .config/nvim/lua/config/plugins/lint.lua
+++ .config/nvim/lua/config/plugins/lint.lua
@@ -1,9 +1,7 @@
 return {
     'mfussenegger/nvim-lint',
     opts = {
-        linters_by_ft = {
-            cpp = { 'clangtidy' },
-        },
+        linters_by_ft = {},
     },
     config = function(_, opts)
         require('lint').linters_by_ft = opts.linters_by_ft
