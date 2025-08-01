79ba762c4900075fd05b22a72744c6faf55379f3
Anton Shabat <anton.shabat@gmail.com>
2025-08-01 18:04:48 +0300

    wezterm enable kitty keyboard protocol

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,28 +5,28 @@ local events = require 'events'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback {
-	'FiraCode Nerd Font Mono',
-	'Unifont',
-	'Unifont Upper',
-	'Unifont CSUR',
+    'FiraCode Nerd Font Mono',
+    'Unifont',
+    'Unifont Upper',
+    'Unifont CSUR',
 }
 config.font_rules = {
-	{
-		italic = true,
-		intensity = 'Normal',
-		font = wezterm.font {
-			family = 'VictorMono',
-			style = 'Italic',
-		},
-	},
+    {
+        italic = true,
+        intensity = 'Normal',
+        font = wezterm.font {
+            family = 'VictorMono',
+            style = 'Italic',
+        },
+    },
 }
 config.font_size = 12.5
 config.harfbuzz_features = events.default_harfbuzz
 
 local colors_file = io.open('/home/anton/.config/wezterm/base16-scheme.toml', 'r')
 if colors_file ~= nil then
-	local colors, _ = wezterm.color.load_scheme '/home/anton/.config/wezterm/base16-scheme.toml'
-	config.colors = colors
+    local colors, _ = wezterm.color.load_scheme '/home/anton/.config/wezterm/base16-scheme.toml'
+    config.colors = colors
 end
 
 config.window_close_confirmation = 'NeverPrompt'
@@ -35,5 +35,7 @@ config.enable_wayland = false
 config.disable_default_key_bindings = true
 config.keys = require('keys').keys
 config.key_tables = require('keys').key_tables
+
+config.enable_kitty_keyboard = true
 
 return config
