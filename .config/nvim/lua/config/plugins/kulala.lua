9d1d292125532bdfd7df984c9ef5e73bfdaba907
Anton Shabat <anton.shabat@gmail.com>
2025-09-20 09:09:36 +0300

    nvim change rest to kulala

---
+++ .config/nvim/lua/config/plugins/kulala.lua
@@ -0,0 +1,17 @@
+return {
+    'mistweaverco/kulala.nvim',
+    ft = { 'http', 'rest' },
+    keys = {
+        { '<leader>Rs', desc = 'Send request' },
+        { '<leader>Ra', desc = 'Send all requests' },
+        { '<leader>Rb', desc = 'Open scratchpad' },
+    },
+    opts = {
+        global_keymaps = true,
+        global_keymaps_prefix = '<leader>R',
+        kulala_keymaps_prefix = '',
+        ui = {
+            display_mode = 'float',
+        },
+    },
+}
