205649ab8b638f915f6bab2845d394e8c483f22c
Anton Shabat <anton.shabat@gmail.com>
2025-11-18 22:35:24 +0200

    nvim set localleader to ','

--- .config/nvim/lua/config/plugins/repl.lua
+++ .config/nvim/lua/config/plugins/repl.lua
@@ -3,7 +3,7 @@ return {
         'Olical/conjure',
         init = function()
             vim.keymap.set({ 'n', 'v' }, ',', '<NOP>')
-            vim.g['conjure#mapping#prefix'] = ','
+            vim.g['conjure#mapping#prefix'] = '<localleader>'
             vim.g['conjure#mapping#doc_word'] = 'K'
         end,
     },
