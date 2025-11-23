d1e5b8f31bbc9862191470a0df5ef5350a985e29
Anton Shabat <anton.shabat@gmail.com>
2025-11-23 16:40:17 +0200

    nvim conjure rewrote :Clj to work without additional plugins

--- .config/nvim/lua/config/plugins/repl.lua
+++ .config/nvim/lua/config/plugins/repl.lua
@@ -6,5 +6,12 @@ return {
             vim.g['conjure#mapping#prefix'] = '<localleader>'
             vim.g['conjure#mapping#doc_word'] = 'K'
         end,
+        config = function()
+            vim.api.nvim_create_user_command('Clj', function()
+                vim.system({ 'clj', '-M:repl/conjure' }, { stdin = true })
+            end, {
+                force = true,
+            })
+        end,
     },
 }
