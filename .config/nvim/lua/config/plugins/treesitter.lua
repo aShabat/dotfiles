fb4cb03978e01bd14e29f1728afaffd1dd95f2cc
Anton Shabat <anton.shabat@gmail.com>
2025-12-28 19:57:17 +0200

    nvim treesitter stuff

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -1,32 +1,19 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
-        main = 'nvim-treesitter.configs',
-        opts = {
-            auto_install = true,
-            ensure_installed = 'all',
-            highlight = {
-                enable = true,
-            },
-            indent = {
-                enable = true,
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    node_incremental = '+',
-                    scope_incremental = '*',
-                    node_decremental = '-',
-                },
-            },
-        },
+        lazy = false,
+        build = ':TSUpdate',
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        branch = 'main',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        init = function()
+            vim.g.no_plugin_maps = true
+        end,
     },
     {
         'nvim-treesitter/nvim-treesitter-context',
-        config = function(_, opts)
-            local context = require 'treesitter-context'
-            context.setup(opts)
-            vim.keymap.set({ 'n', 'v', 'o' }, '<SC-U>', context.go_to_context)
-        end,
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
     },
 }
