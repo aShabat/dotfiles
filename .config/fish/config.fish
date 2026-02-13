a5d4646783d1121cca94866acd4e876dcc38b83e
Anton Shabat <anton.shabat@gmail.com>
2026-02-13 13:50:54 +0200

    fish removed opam config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,11 +16,3 @@ abbr -a snapl sudo snapper list -a --disable-used-space
 abbr -a snapls sudo snapper list -a
 
 set -g fish_key_bindings set_custom_keys
-
-# BEGIN opam configuration
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
-# test -r '/home/anton/.opam/opam-init/init.fish' && source '/home/anton/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
-# END opam configuration
